d6e514008715b8ea857cdc4bf2d1bed9c8acae19
Jasper Schalla <jasper.schalla@web.de>
2024-10-10 22:59:57 +0200

    Refactor main.rs: Remove unused test command

--- src/main.rs
+++ src/main.rs
@@ -163,12 +163,6 @@ fn main() -> Result<()> {
             }
             Ok(())
         }
-        "test" => {
-            conn.execute("DELETE FROM task", params![])?;
-            conn.execute("DELETE FROM SQLITE_SEQUENCE WHERE name='task'", params![])?;
-            println!("{}", "Test command is running...");
-            Ok(())
-        }
         _ => {
             println!("{}", "Invalid action.".bright_red());
             Ok(())
